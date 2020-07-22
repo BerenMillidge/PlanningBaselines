a647ecbbf3a633ad48e07e9590327255cb2ce1b8
Beren <bmillidgework@gmail.com>
2020-07-22 15:05:16 +0100

    update env for new envs

--- env.py
+++ env.py
@@ -25,6 +25,11 @@ import matplotlib
 matplotlib.rcParams["axes.linewidth"] = 1.1
 from envs.planar_env import *
 from envs.piche_env import *
+from envs.lunar_lander import LunarLander, LunarLanderContinuous
+from envs.double_pendulum import SparseDoublePendulum
+from envs.mountain_car import MountainCar
+from envs.pendulum import PendulumEnv
+from envs.acrobot import AcrobotEnv
 
 class Env(object):
     def __init__(self, env_name, max_episode_steps, action_repeat=1, seed=None,device="cpu"):
@@ -70,13 +75,33 @@ class Env(object):
     def dynamics_functions(self, states, actions):
         return self._env.batch_dynamics(states, actions)
 
+    def set_state(self, state):
+        return self._env.set_state(state)
+
+    def get_env_names(self):
+        return ["toy", "piche","pendulum","double_pendulum","lunar_lander","lunar_lander_discrete","mountain_car"]
+
     def _get_env_object(self, env_name):
         if env_name == "toy":
             return PlanarEnv(self.device)
         elif env_name == "piche":
             return PicheEnv(self.device)
+        elif env_name == "pendulum":
+            return PendulumEnv()
+        elif env_name == "double_pendulum":
+            return SparseDoublePendulum()
+        elif env_name == "lunar_lander":
+            return LunarLanderContinuous()
+        elif env_name == "lunar_lander_discrete":
+            return LunarLander()
+        elif env_name == "mountain_car":
+            return MountainCar()
         else:
-            return gym.make(env_name)
+            try: 
+                return gym.make(env_name)
+            except:
+                raise ValueError("Env name not recognised. You inputted: " + str(env_name))
+            
 
     @property
     def observation_space(self):
